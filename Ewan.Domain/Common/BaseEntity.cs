118d7b7641733c512baeb6104b21eabb25e3e64e
Tasnim <tasnim201050@gmail.com>
2026-08-11 22:57:35 +0300

    edit base entity

--- Ewan.Domain/Common/BaseEntity.cs
+++ Ewan.Domain/Common/BaseEntity.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Ewan.Domain.Common;
 
-namespace Ewan.Domain.Common
+public abstract class BaseEntity
 {
-    public class BaseEntity
-    {
-        public int Id { get; set; }
-        public bool IsActive { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
-        public int CreatedByUserId { get; set; }
-        public int? UpdatedByUserId { get; set; }
-    }
+    public int Id { get; set; }
+    public bool IsActive { get; set; } = true;
+    public bool IsDeleted { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+    public int? CreatedByUserId { get; set; }
+    public int? UpdatedByUserId { get; set; }
 }
